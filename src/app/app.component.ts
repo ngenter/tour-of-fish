cafb6c8bd8021952f23d177fc31a27a79ac82da8
Nate Genter <ngenter@plasticingenuity.local>
2016-10-12 10:29:25 -0500

    Completed Members array

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+export class Members { 
+  name: string;
+  age: number;
+}
+
+const FAMILY:  Members [] = [
+  { age: 34, name: 'Kimberly' },
+  { age: 10, name: 'Theresa' },
+  { age: 1,  name: 'Khaleesi'},
+  { age: 30, name: 'Nicholas' }
+
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Tour of Family';
+  name = 'Kimberly';
 }
+
+
